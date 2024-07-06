7adc4fb1710f9d28924b97a9ac7b4d76469450a6
DOMINIC KAKARI OBENG <dkobeng007@st.ug.edu.gh>
2024-07-06 16:12:52 +0000

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Button buttonLogin;
+
+
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        // InitializeComponent method in Form1.Designer.cs
+        private void InitializeComponent()
+        {
+            textBoxUsername = new TextBox();
+            textBoxPassword = new TextBox();
+            buttonLogin = new Button();
+            SuspendLayout();
+            // 
+            // textBoxUsername
+            // 
+            textBoxUsername.Location = new Point(16, 18);
+            textBoxUsername.Margin = new Padding(4, 5, 4, 5);
+            textBoxUsername.Name = "textBoxUsername";
+            textBoxUsername.Size = new Size(132, 27);
+            textBoxUsername.TabIndex = 0;
+            textBoxUsername.PlaceholderText = "Username";
+            // 
+            // textBoxPassword
+            // 
+            textBoxPassword.Location = new Point(16, 58);
+            textBoxPassword.Margin = new Padding(4, 5, 4, 5);
+            textBoxPassword.Name = "textBoxPassword";
+            textBoxPassword.Size = new Size(132, 27);
+            textBoxPassword.TabIndex = 1;
+            textBoxPassword.PlaceholderText = "Password";
+            // 
+            // buttonLogin
+            // 
+            buttonLogin.Location = new Point(16, 98);
+            buttonLogin.Margin = new Padding(4, 5, 4, 5);
+            buttonLogin.Name = "buttonLogin";
+            buttonLogin.Size = new Size(100, 35);
+            buttonLogin.TabIndex = 2;
+            buttonLogin.Text = "Login";
+            buttonLogin.UseVisualStyleBackColor = true;
+            buttonLogin.Click += buttonLogin_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(379, 402);
+            Controls.Add(buttonLogin);
+            Controls.Add(textBoxPassword);
+            Controls.Add(textBoxUsername);
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,18 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            string username = textBoxUsername.Text;
+            string password = textBoxPassword.Text;
+            MessageBox.Show($"Username: {username}\nPassword: {password}");
+        }
+
+    }
+}
