94a91e23d41f3a0842dc76530798cfe37f027113
DOMINIC KAKARI OBENG <dkobeng007@st.ug.edu.gh>
2024-07-07 00:13:36 +0000

    completed

---
+++ DrawingCanvas/Form1.Designer.cs
@@ -0,0 +1,38 @@
+﻿namespace DrawingCanvas
+{
+    partial class Form1
+    {
+        private System.Windows.Forms.Panel panelCanvas;
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelCanvas = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panelCanvas
+            // 
+            this.panelCanvas.Location = new System.Drawing.Point(12, 12);
+            this.panelCanvas.Name = "panelCanvas";
+            this.panelCanvas.Size = new System.Drawing.Size(260, 237);
+            this.panelCanvas.TabIndex = 0;
+            this.panelCanvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelCanvas_MouseDown);
+            this.panelCanvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelCanvas_MouseMove);
+            this.panelCanvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelCanvas_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.panelCanvas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ DrawingCanvas/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DrawingCanvas
+{
+    public partial class Form1 : Form
+    {
+        private bool isDrawing = false;
+        private Point lastPoint;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panelCanvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDrawing = true;
+                lastPoint = e.Location;
+            }
+        }
+
+        private void panelCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                using (Graphics g = panelCanvas.CreateGraphics())
+                {
+                    g.DrawLine(Pens.Black, lastPoint, e.Location);
+                    lastPoint = e.Location;
+                }
+            }
+        }
+
+        private void panelCanvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDrawing = false;
+            }
+        }
+    }
+}
